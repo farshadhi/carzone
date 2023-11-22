4f9ef10aa1b6ea7112225c8c91105b1f4423b88e
farshad <farshadhayati@gmail.com>
2023-11-22 19:21:45 +0330

    created pages and configured navigation links

--- carzone/pages/views.py
+++ carzone/pages/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'pages/index.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def services(request):
+    return render(request, 'pages/services.html')
+
+def contact(request):
+    return render(request, 'pages/contact.html')
